48abe7b79189f6d48f07fd745067f26b61c353ce
wisdom2608 <166314552+wisdom2608@users.noreply.github.com>
2024-07-24 09:56:44 +0100

    Update TerraformExam.tf

--- TerraformExam.tf
+++ TerraformExam.tf
@@ -1,2 +1,2 @@
-Exam Questions
+Terraform Practice Exam Questions | Hashicorp Certified Terraform Associate Practice Test
 #Reference 1: https://youtu.be/obBSeSgQ1fM?si=IID2Ct7W72I1vD2d (Champ Cloud)
