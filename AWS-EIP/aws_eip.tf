aafba2a805065071c24bb364ca1e744ead5d49df
A Julius <166314552+wisdom2608@users.noreply.github.com>
2025-06-26 12:47:02 +0100

    Create aws_eip.tf

---
+++ AWS-EIP/aws_eip.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"  # Change this to your desired AWS region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c94855ba95c71c99"  # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
+
+resource "aws_eip" "example" {
+  vpc = true
+}
+
+resource "aws_eip_association" "example" {
+  instance_id   = aws_instance.example.id
+  allocation_id = aws_eip.example.id
+}
